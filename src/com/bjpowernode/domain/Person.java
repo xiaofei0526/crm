f9ca4c8039a55eb7c9e925aff64caa5c9a34a6ef
xiaofei0526 <459482735@qq.com>
2020-06-02 20:40:58 +0800

    111

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description:
+ *
+ * @date:2020/6/2 20:34
+ * @author:xiao
+ */
+public class Person {
+    private String id;
+
+}
